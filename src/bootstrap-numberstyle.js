28e101e68aad1947639d40348bdf2f4b22fcaf19
Markus Lima <markusslima@gmail.com>
2018-06-22 17:09:31 -0300

    Fixed all methods

---
+++ src/bootstrap-numberstyle.js
@@ -0,0 +1,235 @@
+/*
+ * Bootstrap Number Style
+ * Docs: http://markusslima.github.io/bootstrap-number-style/
+ * github: https://github.com/markusslima/bootstrap-number-style
+ * Copyright (c) 2018 Markus Vinicius da Silva Lima
+ * Version 1.0.0
+ * Licensed under the MIT license.
+ */
+(function($) {
+    "use strict";
+
+    var nextId = 0;
+
+    var BNumberStyle = function(element, options) {
+        this.options = options;
+        this.$elementBNumberStyle = [];
+        this.$element = $(element);
+    };
+
+    BNumberStyle.prototype = {
+        destroy : function() {
+            var clone = this.$element.clone();
+            this.$element.remove();
+            this.$elementBNumberStyle.before(clone);
+            this.$elementBNumberStyle.remove();
+            this.$element.removeData('bNumberStyle');
+        },
+
+        min : function (value) {
+            if (value != undefined) {
+                this.options.min = value;
+                if (this.$element.val() < this.options.min) {
+                    this.$element.val(this.options.min)
+                }
+            } else {
+                return this.options.min;
+            }
+        },
+
+        max : function (value) {
+            if (value != undefined) {
+                this.options.max = value;
+                if (this.$element.val() > this.options.max) {
+                    this.$element.val(this.options.max)
+                }
+            } else {
+                return this.options.max;
+            }
+        },
+
+        step : function (value) {
+            if (value != undefined) {
+                this.options.step = value;
+            } else {
+                return this.options.step;
+            }
+        },
+
+        onPlusBefore : function (value) {
+            if (value !== undefined) {
+                if (typeof value == 'function') {
+                    this.options.onPlusBefore = value;
+                } else {
+                    console.error('This "onPlusBefore" not a function.')
+                }
+            } else {
+                return this.options.onPlusBefore;
+            }
+        },
+
+        onPlusAfter : function (value) {
+            if (value !== undefined) {
+                if (typeof value == 'function') {
+                    this.options.onPlusAfter = value;
+                } else {
+                    console.error('This "onPlusAfter" not a function.')
+                }
+            } else {
+                return this.options.onPlusAfter;
+            }
+        },
+
+        onMinusBefore : function (value) {
+            if (value !== undefined) {
+                if (typeof value == 'function') {
+                    this.options.onMinusBefore = value;
+                } else {
+                    console.error('This "onMinusBefore" not a function.')
+                }
+            } else {
+                return this.options.onMinusBefore;
+            }
+        },
+
+        onMinusAfter : function (value) {
+            if (value !== undefined) {
+                if (typeof value == 'function') {
+                    this.options.onMinusAfter = value;
+                } else {
+                    console.error('This "onMinusAfter" not a function.')
+                }
+            } else {
+                return this.options.onMinusAfter;
+            }
+        },
+
+        disabled : function (value) {
+            if (value != undefined) {
+                this.options.disabled = value;
+
+                if (this.options.disabled) {
+                    this.$element.prop('disabled', true);
+                    this.$elementBNumberStyle.addClass('disabled');
+                } else {
+                    this.$element.prop('disabled', false);
+                    this.$elementBNumberStyle.removeClass('disabled');
+                }
+            } else {
+                return this.options.disabled;
+            }
+        },
+
+        constructor : function() {
+            var _self = this,
+                clone = this.$element.clone();
+            
+            this.$elementBNumberStyle = $('<div class="bNumberStyle"></div>')
+                            .append(clone)
+                            .append('<div class="bNumberStyle-nav"><div class="bNumberStyle-button bNumberStyle-up">+</div><div class="bNumberStyle-button bNumberStyle-down">-</div></div>');
+
+            this.$element.before(this.$elementBNumberStyle);
+            this.$element.remove();
+
+            this.$element = clone;
+
+            if (this.options.min == '' && this.$element.attr('min') != '') {
+                this.options.min = this.$element.attr('min');
+            }
+            if (this.options.max == '' && this.$element.attr('max') != '') {
+                this.options.max = this.$element.attr('max');
+            }
+            if (this.options.step == 1 && this.$element.attr('step') != '') {
+                this.options.step = this.$element.attr('step');
+            }
+
+            if (this.options.disabled) {
+                this.$elementBNumberStyle.addClass('disabled');
+                this.$element.prop('disabled', true);
+            } else {
+                this.$elementBNumberStyle.removeClass('disabled');
+                this.$element.prop('disabled', false);
+            }
+
+            this.$element.on('keydown', function (e) {
+                var k = e.keyCode;
+                if ((k < 48 || k > 57) && (k < 96 || k > 105) && !String(k).match(/^(8|9|38|40|46)/)) {
+                    return false;
+                } else if (!String(k).match(/^(8|9|38|40|46)/)) {
+                    if (_self.options.max > 0) {
+                        if (Number($(this).val()) + Number(String.fromCharCode(57)) > _self.options.max) {
+                            return false;
+                        }
+                    }
+                }
+            });
+
+            this.$elementBNumberStyle.find('.bNumberStyle-up').on('click', function () {
+                _self.options.onPlusBefore();
+                var oldValue = parseFloat(_self.$element.val());
+                if (oldValue >= _self.options.max && _self.options.max != '') {
+                    var newVal = oldValue;
+                } else {
+                    var newVal = oldValue + parseFloat(_self.options.step);
+                }
+                _self.$element
+                    .val(newVal)
+                    .trigger("change");
+                _self.options.onPlusAfter();
+            });
+
+            this.$elementBNumberStyle.find('.bNumberStyle-down').on('click', function () {
+                _self.options.onMinusBefore();
+                var oldValue = parseFloat(_self.$element.val());
+                if (oldValue <= _self.options.min && _self.options.max != '') {
+                    var newVal = oldValue;
+                } else {
+                    var newVal = oldValue - parseFloat(_self.options.step);
+                }
+                _self.$element
+                    .val(newVal)
+                    .trigger("change");
+                _self.options.onMinusAfter();
+            });
+        }
+    };
+
+    var old = $.fn.bNumberStyle;
+
+    $.fn.bNumberStyle = function(option, value) {
+        var get = '', element = this.each(function() {
+            var $this = $(this), data = $this.data('bNumberStyle'), options = $.extend({}, $.fn.bNumberStyle.defaults, option, typeof option === 'object' && option);
+
+            if (!data) {
+                $this.data('bNumberStyle', ( data = new BNumberStyle(this, options)));
+                data.constructor();
+            }
+
+            if ( typeof option === 'string') {
+                get = data[option](value);
+            }
+        });
+
+        if ( typeof get !== undefined) {
+            return get;
+        } else {
+            return element;
+        }
+    };
+
+    $.fn.bNumberStyle.defaults = {
+        'min' : '',
+        'max' : '',
+        'step' : 1,
+        'disabled' : false,
+        'onPlusBefore' : function () {},
+        'onPlusAfter' : function () {},
+        'onMinusBefore' : function () {},
+        'onMinusAfter' : function () {}
+    };
+
+    $.fn.bNumberStyle.noConflict = function() {
+        $.fn.bNumberStyle = old;
+        return this;
+    };
+})(window.jQuery);
